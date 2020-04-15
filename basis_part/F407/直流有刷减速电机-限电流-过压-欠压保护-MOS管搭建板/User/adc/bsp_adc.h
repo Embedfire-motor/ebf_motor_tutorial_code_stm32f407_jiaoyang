9171e5af2b058d9bc10291475e2d6baa35b1a83c
LONG <1558193230@qq.com>
2020-04-15 17:11:47 +0800

    增加电流电压读取代码，双电机控制代码

---
+++ basis_part/F407/直流有刷减速电机-限电流-过压-欠压保护-MOS管搭建板/User/adc/bsp_adc.h
@@ -0,0 +1,63 @@
+#ifndef __BSP_ADC_H
+#define	__BSP_ADC_H
+
+#include "stm32f4xx.h"
+
+// ADC 序号宏定义
+#define CURR_ADC                        ADC1
+#define CURR_ADC_CLK_ENABLE()           __ADC1_CLK_ENABLE()
+
+#define ADC_VBUS_IRQ                     ADC_IRQn
+#define ADC_VBUS_IRQHandler              ADC_IRQHandler
+
+#define VREF                            3.248f     // 参考电压，理论上是3.3，可通过实际测量得3.258
+#define ADC_NUM_MAX                     2048       // ADC 转换结果缓冲区最大值
+
+#define GET_ADC_VDC_VAL(val)            ((float)val/(float)4096*VREF)          // 得到电压值
+  
+/*********************** 电流采集 ******************/
+// ADC GPIO 宏定义
+#define CURR_ADC_GPIO_PORT              GPIOB
+#define CURR_ADC_GPIO_PIN               GPIO_PIN_1
+#define CURR_ADC_GPIO_CLK_ENABLE()      __GPIOB_CLK_ENABLE()
+
+#define CURR_ADC_CHANNEL                ADC_CHANNEL_9
+
+// ADC DR寄存器宏定义，ADC转换后的数字值则存放在这里
+#define CURR_ADC_DR_ADDR                ((uint32_t)ADC1+0x4c)
+
+// ADC DMA 通道宏定义，这里我们使用DMA传输
+#define CURR_ADC_DMA_CLK_ENABLE()       __DMA2_CLK_ENABLE()
+#define CURR_ADC_DMA_CHANNEL            DMA_CHANNEL_0
+#define CURR_ADC_DMA_STREAM             DMA2_Stream0
+
+#define ADC_DMA_IRQ                     DMA2_Stream0_IRQn
+#define ADC_DMA_IRQ_Handler             DMA2_Stream0_IRQHandler
+
+#define GET_ADC_CURR_VAL(val)           ((float)val/(float)10.0/(float)0.02*(float)1000.0)          // 得到电流值，电压放大10倍，0.02是采样电阻，单位mA。
+
+/*********************** 电源电压采集 ******************/
+
+#define VBUS_GPIO_PORT                  GPIOB
+#define VBUS_GPIO_PIN                   GPIO_PIN_0
+#define VBUS_GPIO_CLK_ENABLE()          __GPIOB_CLK_ENABLE()
+
+#define VBUS_ADC_CHANNEL                ADC_CHANNEL_8
+
+#define VBUS_MAX                        14    // 电压最大值
+#define VBUS_MIN                        10    // 电压最小值
+
+#define VBUS_HEX_MAX                    (VBUS_MAX/21.615/VREF*4096)    // 电压最大值（测量电压是电源电压的1/31）31.0
+#define VBUS_HEX_MIN                    (VBUS_MIN/21.615/VREF*4096)    // 电压最小值（测量电压是电源电压的1/31）31.0
+
+extern DMA_HandleTypeDef DMA_Init_Handle;
+extern ADC_HandleTypeDef ADC_Handle;
+
+void ADC_Init(void);
+int32_t get_curr_val(void);
+float get_vbus_val(void);
+
+#endif /* __BSP_ADC_H */
+
+
+
---
+++ basis_part/F407/直流有刷减速电机-限电流-过压-欠压保护-MOS管搭建板/User/motor_control/bsp_motor_control.c
@@ -0,0 +1,89 @@
+/**
+  ******************************************************************************
+  * @file    bsp_motor_control.c
+  * @author  fire
+  * @version V1.0
+  * @date    2019-xx-xx
+  * @brief   电机控制接口
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火  STM32 F407 开发板 
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+  */ 
+
+#include ".\motor_control\bsp_motor_control.h"
+
+/* 私有变量 */
+static motor_dir_t direction  = MOTOR_FWD;     // 记录方向
+static uint16_t    dutyfactor = 0;             // 记录占空比
+
+/**
+  * @brief  设置电机速度
+  * @param  v: 速度（占空比）
+  * @retval 无
+  */
+void set_motor_speed(uint16_t v)
+{
+  dutyfactor = v;
+  
+  if (direction == MOTOR_FWD)
+  {
+    SET_FWD_COMPAER(dutyfactor);     // 设置速度
+  }
+  else
+  {
+    SET_REV_COMPAER(dutyfactor);     // 设置速度
+  }
+}
+
+/**
+  * @brief  设置电机方向
+  * @param  无
+  * @retval 无
+  */
+void set_motor_direction(motor_dir_t dir)
+{
+  direction = dir;
+  
+  if (direction == MOTOR_FWD)
+  {
+    SET_FWD_COMPAER(dutyfactor);     // 设置速度
+    SET_REV_COMPAER(0);              // 设置速度
+  }
+  else
+  {
+    SET_FWD_COMPAER(0);              // 设置速度
+    SET_REV_COMPAER(dutyfactor);     // 设置速度
+  }
+}
+
+/**
+  * @brief  使能电机
+  * @param  无
+  * @retval 无
+  */
+void set_motor_enable(void)
+{
+  HAL_GPIO_WritePin(SD_GPIO_PORT, SD_PIN, GPIO_PIN_RESET);    // SD 脚输出高电平使能
+  MOTOR_FWD_ENABLE();
+  MOTOR_REV_ENABLE();
+}
+
+/**
+  * @brief  禁用电机
+  * @param  无
+  * @retval 无
+  */
+void set_motor_disable(void)
+{
+  HAL_GPIO_WritePin(SD_GPIO_PORT, SD_PIN, GPIO_PIN_SET);    // SD 脚输出高电平禁用
+  MOTOR_FWD_DISABLE();
+  MOTOR_REV_DISABLE();
+}
+
+/*********************************************END OF FILE**********************/
+
