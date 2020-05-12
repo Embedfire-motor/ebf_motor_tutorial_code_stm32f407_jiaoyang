c6af436ee297dabf294a24fb80e09afc0b7befb2
LONG <1558193230@qq.com>
2020-05-12 11:58:36 +0800

    增加-有刷-电流环-位置式-控制

--- improve_part/F407/直流有刷电机-电流环控制-位置式PID/User/motor_control/bsp_motor_control.c
+++ improve_part/F407/直流有刷电机-电流环控制-位置式PID/User/motor_control/bsp_motor_control.c
@@ -16,11 +16,16 @@
   */ 
 
 #include ".\motor_control\bsp_motor_control.h"
-#include "./led/bsp_led.h"   
+#include "./usart/bsp_debug_usart.h"
+#include "./pid/bsp_pid.h"
+#include "./tim/bsp_basic_tim.h"
+#include "./adc/bsp_adc.h"
+#include <math.h>
+#include <stdlib.h>
 
-/* 私有变量 */
 static motor_dir_t direction  = MOTOR_FWD;     // 记录方向
 static uint16_t    dutyfactor = 0;             // 记录占空比
+static uint8_t    is_motor_en = 0;             // 电机使能
 
 static void sd_gpio_config(void)
 {
@@ -59,6 +64,8 @@ void motor_init(void)
   */
 void set_motor_speed(uint16_t v)
 {
+  v = (v > PWM_PERIOD_COUNT) ? PWM_PERIOD_COUNT : v;     // 上限处理
+  
   dutyfactor = v;
   
   if (direction == MOTOR_FWD)
@@ -80,6 +87,11 @@ void set_motor_direction(motor_dir_t dir)
 {
   direction = dir;
   
+//  SET_FWD_COMPAER(0);     // 设置速度为 0
+//  SET_REV_COMPAER(0);     // 设置速度为 0
+
+//  HAL_Delay(200);         // 延时一会
+  
   if (direction == MOTOR_FWD)
   {
     SET_FWD_COMPAER(dutyfactor);     // 设置速度
@@ -90,6 +102,7 @@ void set_motor_direction(motor_dir_t dir)
     SET_FWD_COMPAER(0);              // 设置速度
     SET_REV_COMPAER(dutyfactor);     // 设置速度
   }
+  
 }
 
 /**
@@ -99,7 +112,8 @@ void set_motor_direction(motor_dir_t dir)
   */
 void set_motor_enable(void)
 {
-  MOTOR_ENABLE_SD();    // SD 脚输出高电平使能
+  is_motor_en = 1;
+  MOTOR_ENABLE_SD();
   MOTOR_FWD_ENABLE();
   MOTOR_REV_ENABLE();
 }
@@ -111,9 +125,61 @@ void set_motor_enable(void)
   */
 void set_motor_disable(void)
 {
-  MOTOR_DISABLE_SD();    // SD 脚输出高电平禁用
+  is_motor_en = 0;
+  MOTOR_DISABLE_SD();
   MOTOR_FWD_DISABLE();
   MOTOR_REV_DISABLE();
+}
+
+/**
+  * @brief  电机位置式 PID 控制实现(定时调用)
+  * @param  无
+  * @retval 无
+  */
+void motor_pid_control(void)
+{
+  int32_t actual_current = get_curr_val();    // 读取当前电流值
+  
+  if (is_motor_en == 1)     // 电机在使能状态下才进行控制处理
+  {
+    float cont_val = 0;                       // 当前控制值
+    static __IO int32_t Capture_Count = 0;    // 当前时刻总计数值
+    static __IO int32_t Last_Count = 0;       // 上一时刻总计数值
+
+    cont_val = PID_realize(actual_current);    // 进行 PID 计算
+    
+    if (cont_val > 0)    // 判断电机方向
+    {
+      //set_motor_direction(MOTOR_FWD);
+    }
+    else
+    {
+      cont_val = -cont_val;
+      //set_motor_direction(MOTOR_REV);
+    }
+    
+    cont_val = (cont_val > PWM_MAX_PERIOD_COUNT) ? PWM_MAX_PERIOD_COUNT : cont_val;    // 速度上限处理
+    set_motor_speed(cont_val);                                                 // 设置 PWM 占空比
+    
+  #if defined(PID_ASSISTANT_EN)
+    set_computer_value(SEND_FACT_CMD, CURVES_CH1, &actual_current, 1);                // 给通道 1 发送实际值
+  #else
+    printf("实际值：%d. 目标值：%.0f\n", actual_speed, get_pid_actual());      // 打印实际值和目标值
+  #endif
+  }
+}
+
+/**
+  * @brief  定时器每100ms产生一次中断回调函数
+  * @param  htim：定时器句柄
+  * @retval 无
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if(htim==(&TIM_TimeBaseStructure))
+  {
+    motor_pid_control();
+  }
 }
 
 /*********************************************END OF FILE**********************/
