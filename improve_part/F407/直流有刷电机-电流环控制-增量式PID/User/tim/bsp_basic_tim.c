0bde10881cbf3d997f5c3153672db47044cb2519
LONG <1558193230@qq.com>
2020-05-12 12:03:52 +0800

    增加-有刷-电流环-增量式控制（复制位置式）

---
+++ improve_part/F407/直流有刷电机-电流环控制-增量式PID/User/tim/bsp_basic_tim.c
@@ -0,0 +1,91 @@
+/**
+  ******************************************************************************
+  * @file    bsp_basic_tim.c
+  * @author  STMicroelectronics
+  * @version V1.0
+  * @date    2015-xx-xx
+  * @brief   基本定时器定时范例
+  ******************************************************************************
+  * @attention
+  *
+  * 实验平台:野火  STM32 F407 开发板  
+  * 论坛    :http://www.firebbs.cn
+  * 淘宝    :http://firestm32.taobao.com
+  *
+  ******************************************************************************
+  */
+  
+#include "./tim/bsp_basic_tim.h"
+#include "./usart/bsp_debug_usart.h"
+
+TIM_HandleTypeDef TIM_TimeBaseStructure;
+ /**
+  * @brief  基本定时器 TIMx,x[6,7]中断优先级配置
+  * @param  无
+  * @retval 无
+  */
+static void TIMx_NVIC_Configuration(void)
+{
+	//设置抢占优先级，子优先级
+	HAL_NVIC_SetPriority(BASIC_TIM_IRQn, 1, 3);
+	// 设置中断来源
+	HAL_NVIC_EnableIRQ(BASIC_TIM_IRQn);
+}
+
+/*
+ * 注意：TIM_TimeBaseInitTypeDef结构体里面有5个成员，TIM6和TIM7的寄存器里面只有
+ * TIM_Prescaler和TIM_Period，所以使用TIM6和TIM7的时候只需初始化这两个成员即可，
+ * 另外三个成员是通用定时器和高级定时器才有.
+ *-----------------------------------------------------------------------------
+ * TIM_Prescaler         都有
+ * TIM_CounterMode			 TIMx,x[6,7]没有，其他都有（基本定时器）
+ * TIM_Period            都有
+ * TIM_ClockDivision     TIMx,x[6,7]没有，其他都有(基本定时器)
+ * TIM_RepetitionCounter TIMx,x[1,8]才有(高级定时器)
+ *-----------------------------------------------------------------------------
+ */
+static void TIM_Mode_Config(void)
+{
+	// 开启TIMx_CLK,x[6,7] 
+	BASIC_TIM_CLK_ENABLE(); 
+
+	TIM_TimeBaseStructure.Instance = BASIC_TIM;
+	/* 累计 TIM_Period个后产生一个更新或者中断*/		
+	//当定时器从0计数到BASIC_PERIOD_COUNT-1，即为BASIC_PERIOD_COUNT次，为一个定时周期
+	TIM_TimeBaseStructure.Init.Period = BASIC_PERIOD_COUNT - 1;       
+
+	//定时器时钟源TIMxCLK = 2 * PCLK1  
+	//				PCLK1 = HCLK / 4 
+	//				=> TIMxCLK=HCLK/2=SystemCoreClock/2=84MHz
+	// 设定定时器频率为=TIMxCLK/BASIC_PRESCALER_COUNT
+	TIM_TimeBaseStructure.Init.Prescaler = BASIC_PRESCALER_COUNT - 1;	
+  TIM_TimeBaseStructure.Init.CounterMode = TIM_COUNTERMODE_UP;           // 向上计数
+  TIM_TimeBaseStructure.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;     // 时钟分频
+
+	// 初始化定时器TIMx, x[2,3,4,5]
+	HAL_TIM_Base_Init(&TIM_TimeBaseStructure);
+
+	// 开启定时器更新中断
+	HAL_TIM_Base_Start_IT(&TIM_TimeBaseStructure);	
+}
+
+/**
+  * @brief  初始化基本定时器定时，1ms产生一次中断
+  * @param  无
+  * @retval 无
+  */
+void TIMx_Configuration(void)
+{
+	TIMx_NVIC_Configuration();	
+  
+	TIM_Mode_Config();
+  
+#if defined(PID_ASSISTANT_EN)
+  uint32_t temp = GET_BASIC_TIM_PERIOD();     // 计算周期，单位ms
+  
+  set_computer_value(SEND_PERIOD_CMD, CURVES_CH1, &temp, 1);     // 给通道 1 发送目标值
+#endif
+
+}
+
+/*********************************************END OF FILE**********************/
