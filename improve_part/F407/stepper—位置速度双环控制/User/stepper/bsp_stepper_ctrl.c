49106d8c6fe3073f0c3080523c65fabbb1b47de4
hejiawen <hjw0415@outlook.com>
2020-06-18 14:39:35 +0800

    完善注释

--- improve_part/F407/stepper—位置速度双环控制/User/stepper/bsp_stepper_ctrl.c
+++ improve_part/F407/stepper—位置速度双环控制/User/stepper/bsp_stepper_ctrl.c
@@ -93,16 +93,17 @@ void Stepper_Speed_Ctrl(void)
   {
     /* 计算编码器脉冲数 */
     capture_count = (int)__HAL_TIM_GET_COUNTER(&TIM_EncoderHandle) + (encoder_overflow_count * ENCODER_TIM_PERIOD);
-		
+		/* 计算速度环的传入值 */
 		capture_per_unit = capture_count - last_count;
     last_count = capture_count;
 		
-		/* 编码器脉冲累计值作为实际值传入位置pid控制器 */
+		/* 编码器脉冲累计值作为实际值传入位置环pid控制器 */
     move_cont_val += PID_realize_move(&move_pid, (float)capture_count);// 进行 PID 计算
     /* 判断运动方向 */
     move_cont_val > 0 ? (MOTOR_DIR(CW)) : (MOTOR_DIR(CCW));
-    /* 计算得出的期望值取绝对值 */
+    /* 传递位置环计算值，便于计算*/
     cont_val = move_cont_val;
+		
 		/* 目标速度上限处理 */
 		if (cont_val > TARGET_SPEED_MAX)
 		{
@@ -112,15 +113,17 @@ void Stepper_Speed_Ctrl(void)
 		{
 			cont_val = -TARGET_SPEED_MAX;
 		}
+		
 #if defined(PID_ASSISTANT_EN)
-      int32_t temp = cont_val;
-      set_computer_value(SEED_TARGET_CMD, CURVES_CH2, &temp, 1);     // 给通道 2 发送目标值
+		int32_t temp = cont_val;
+		set_computer_value(SEED_TARGET_CMD, CURVES_CH2, &temp, 1);     // 给通道 2 发送目标值
 #endif
-		set_pid_target(&speed_pid, cont_val);    // 设定速度的目标值
-    /* 单位时间内的编码器脉冲数作为实际值传入pid控制器 */
+		/* 设定速度的目标值 */
+		set_pid_target(&speed_pid, cont_val);    
+    /* 单位时间内的编码器脉冲数作为实际值传入速度环pid控制器 */
     speed_cont_val += PID_realize_speed(&speed_pid, (float)capture_per_unit);// 进行 PID 计算
+		/* 由于OC_Pulse_num为uint16_t变量，取速度环输出值的绝对值进行后续计算*/
 		cont_val = fabsf(speed_cont_val);	
-//    cont_val >= 40 ? (cont_val = 40) : cont_val;		
     /* 计算比较计数器的值 */
     OC_Pulse_num = ((uint16_t)(TIM_STEP_FREQ / (cont_val * PULSE_RATIO * SAMPLING_PERIOD))) >> 1;
     
@@ -137,7 +140,6 @@ void Stepper_Speed_Ctrl(void)
   else
   {
     /*停机状态所有参数清零*/
-//    OC_Pulse_num = 0xFFFF;
     last_count = 0;
     speed_cont_val = 0;
     move_cont_val = 0;
